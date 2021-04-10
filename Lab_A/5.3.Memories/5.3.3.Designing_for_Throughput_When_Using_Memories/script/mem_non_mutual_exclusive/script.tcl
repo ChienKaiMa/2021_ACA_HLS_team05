f9c48cbbedf893f2f02c31f78907cf26b60f7a12
jchen8tw <1806483+jchen8tw@users.noreply.github.com>
2021-04-11 00:00:17 +0800

    add rpt and script

---
+++ Lab_A/5.3.Memories/5.3.3.Designing_for_Throughput_When_Using_Memories/script/mem_non_mutual_exclusive/script.tcl
@@ -0,0 +1,18 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 1986-2019 Xilinx, Inc. All Rights Reserved.
+############################################################
+open_project project5.3.3.1
+set_top accumulate
+add_files ../github/2021_ACA_HLS_team05/Lab_A/5.3.Memories/5.3.3.Designing_for_Throughput_When_Using_Memories/src/mem_non_mutual_exclusive.h
+add_files ../github/2021_ACA_HLS_team05/Lab_A/5.3.Memories/5.3.3.Designing_for_Throughput_When_Using_Memories/src/mem_non_mutual_exclusive.cpp
+add_files -tb ../github/2021_ACA_HLS_team05/Lab_A/5.3.Memories/5.3.3.Designing_for_Throughput_When_Using_Memories/src/mem_non_mutual_exclusive_test.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+open_solution "solution1"
+set_part {xc7z020clg484-1}
+create_clock -period 5 -name default
+source "./project5.3.3.1/solution1/directives.tcl"
+csim_design
+csynth_design
+cosim_design -trace_level all
+export_design -format ip_catalog
